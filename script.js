ffd5425a1e460cd8332a3c2de5b51f0483469eb9
Pálóczi Imre <palocziimi@gmail.com>
2022-11-06 15:11:47 +0100

    kezdolap kesz?

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+//sötétmód
+function darkmode(){
+    var body = document.getElementById('body')
+    var sotet = document.getElementById('ejjel');
+    var vilagos = document.getElementById('nappal');
+    var napi_gomb = document.getElementById('napi-gomb');
 
+    if(sotet.classList.contains('deactive')){
+        sotet.classList.replace('deactive', 'active');
+        vilagos.classList.replace('active', 'deactive');
+        body.classList.remove('active');
+        napi_gomb.classList.remove('active');
+    }else{
+        vilagos.classList.replace('deactive', 'active');
+        sotet.classList.replace('active', 'deactive');
+        body.classList.add('active');
+        napi_gomb.classList.add('active');
+    }
+}
 
-
+//Scroll
+//ScrollReveal().reveal('.fooldal-szoveg, .fooldal-kep, .gomb', {delay: 200, easing: "eas-out", interval: 200, scale: 1.2})
 
